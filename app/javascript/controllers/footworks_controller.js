1910e54255c1617c67c7555657e4591cebf76f05
adamhannadev <adamhannadev@gmail.com>
2024-08-29 15:23:10 -0700

    Enable row hover active classes on footworks_controller.js

--- app/javascript/controllers/footworks_controller.js
+++ app/javascript/controllers/footworks_controller.js
@@ -3,10 +3,16 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="footworks"
 export default class extends Controller {
   static targets = [ "db"]
+
   connect() {
-    console.log("Hello, Stimulus!", this.element)
-    this.dbTargets.forEach(db => {
-      db.innerHTML = "Changed"
-    });
+
+  }
+
+  inactive(event){
+    event.currentTarget.classList.remove("active")
+  }
+
+  active(event){
+    event.currentTarget.classList.add("active")
   }
 }
