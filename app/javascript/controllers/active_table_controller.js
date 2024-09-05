d7e47c23321d56332be818a858772e48f0457343
adamhannadev <adamhannadev@gmail.com>
2024-09-05 09:46:57 -0700

    Abstract footworks stimulus logic into active table stimulus controller

--- app/javascript/controllers/active_table_controller.js
+++ app/javascript/controllers/active_table_controller.js
@@ -2,11 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="footworks"
 export default class extends Controller {
-  static targets = [ "db"]
-
-  connect() {
-
-  }
 
   inactive(event){
     event.currentTarget.classList.remove("active")
