9d45a83e30ba7961c5557f94e699f1f029ce1aff
adamhannadev <adamhannadev@gmail.com>
2024-09-28 16:44:46 -1000

    Add stimulus controller to hide new step form in component form

--- app/javascript/controllers/fade_controller.js
+++ app/javascript/controllers/fade_controller.js
@@ -3,7 +3,7 @@ import { fadeOut } from "../utilities/fade_out";
 
 // Connects to data-controller="message"
 export default class extends Controller {
-  static targets = ["fadeItem"]
+  static targets = []
   close(){
     fadeOut(this.element);
   }
--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -1,20 +1,30 @@
 // app/javascript/controllers/nested_form_controller.js
-import { Controller } from "stimulus"
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["container", "template"]
-
-  add(event) {
-    
-    event.preventDefault()
-    const content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime())
-    this.containerTarget.insertAdjacentHTML('beforeend', content)
+  static targets = ["new"]
+  
+  connect(){
+    this.newTarget.style.display = "none";
   }
 
-//   remove(event) {
-//     event.preventDefault()
-//     let item = event.target.closest(".nested-fields")
-//     item.querySelector("input[name*='_destroy']").value = 1
-//     item.style.display = 'none'
-//   }
+  showNew(){
+    this.newTarget.style.display = "block";
+  }
+
+
+  // add(event) {
+
+    
+  //   event.preventDefault()
+  //   const content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime())
+  //   this.containerTarget.insertAdjacentHTML('beforeend', content)
+  // }
+
+  // remove(event) {
+  //   event.preventDefault()
+  //   let item = event.target.closest(".nested-fields")
+  //   item.querySelector("input[name*='_destroy']").value = 1
+  //   item.style.display = 'none'
+  // }
 }
