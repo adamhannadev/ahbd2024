a4f60d6e736cc8d014d72862e56b750ca59a7711
adamhannadev <adamhannadev@gmail.com>
2024-12-04 15:02:24 -0800

    Add lessons_taught and lessons_taken queries to contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,5 +5,6 @@ class Contact < ApplicationRecord
 
     validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
     
-    has_many :lessons      
+    has_many :lessons_taught, class_name: 'Lesson', foreign_key: :teacher_id
+    has_many :lessons_taken, class_name: 'Lesson', foreign_key: :student_id    
 end
